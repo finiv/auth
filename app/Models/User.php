c220a8bd7c4723a50d04987ccc718165d26e6167
niasha <niasha@rva.solutions>
2023-08-25 13:56:18 +0300

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,4 +21,22 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'hashed',
+    ];
 }
